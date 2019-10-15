85b4c0f300f3ac0dc6528951d3fe6f5c0475f58d
Megan <mennis0145@gmail.com>
2019-10-15 14:26:28 +0100

    Friends component renders list of friends if jwt in local storage (user logged in) or else alerts error message

--- friends/src/components/Friends.jsx
+++ friends/src/components/Friends.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import withAuth from "../axios";
 
 export default function Friends(props) {
-    return(
-        <div>
-            Friends
-        </div>
-    );
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    withAuth()
+      .get("http://localhost:5000/api/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => {
+        alert(err.response.data.error)
+      });
+  }, []);
+
+  return (
+    <div>
+      {friends.map(friend => {
+       return <div>
+          <h3>Name:{friend.name}</h3>
+          <p>Age:{friend.age}</p>
+          <p>Email:{friend.email}</p>
+        </div>;
+      })}
+    </div>
+  );
 }
