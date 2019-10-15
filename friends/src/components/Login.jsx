c7ff4fd0da84f7a98db6eaf78391fa349bf7d3e3
Megan <mennis0145@gmail.com>
2019-10-15 13:13:03 +0100

    Axios wrapper created to get header

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
 
 export default function Login(props){
+    const userNameRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(userNameRef, passwordRef);
+    }
+
     return(
         <div>
             <form>
-                <input placeholder='Username' />
-                <input placeholder='Password' />
-                <button>Submit</button>
+                <input ref={userNameRef} placeholder='Username' type='text' />
+                <input ref={passwordRef} placeholder='Password' type='text' />
+                <button onClick={onSubmit} >Submit</button>
             </form>
         </div>
     );
