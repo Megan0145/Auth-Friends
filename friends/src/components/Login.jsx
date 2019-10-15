c8d8714a9c48fc8823a142396fcb0daadb894c7f
Megan <mennis0145@gmail.com>
2019-10-15 13:22:46 +0100

    Token saving to local storage on successful login

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -7,7 +7,18 @@ export default function Login(props){
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(userNameRef, passwordRef);
+        console.log(userNameRef.current.value, passwordRef.current.value);
+        axios.post('http://localhost:5000/api/login', {
+            username: userNameRef.current.value,
+            password: passwordRef.current.value
+        })
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.payload)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     return(
